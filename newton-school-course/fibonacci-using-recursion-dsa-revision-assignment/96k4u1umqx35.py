f9405fb9189d0d0228d16322774f7d2a8f624ffd
Ayush Singh <studentuse33@gmail.com>
2026-08-09 03:04:07 +0530

    Sync Solution: Fibonacci Using Recursion - DSA Revision Assignment [Score: 100%]

---
+++ newton-school-course/fibonacci-using-recursion-dsa-revision-assignment/96k4u1umqx35.py
@@ -0,0 +1,11 @@
+# Your code here
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    print(fibonacci(n))
