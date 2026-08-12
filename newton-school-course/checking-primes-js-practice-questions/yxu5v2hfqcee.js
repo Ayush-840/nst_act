d12ba48e7c551f4d28af0bd6973f3c1b2c74e4c3
Ayush Singh <studentuse33@gmail.com>
2026-08-13 02:10:16 +0530

    Sync Solution: Checking Primes - JS Practice Questions [Score: 100%]
    You are given an array of integers, and your task is to determine whether each number is a prime number or not. You assigned this task to your junior, he wrote the following code but it contains an error. You are tasked to debug the errors and correct the function.
    
    ### Input
    The first line of input contains an array of numbers
    
    ### Output
    The first line of output shows an array containing a list if a number is Prime or not
    
    ### Example
    Input:
    [2, 4, 5, 6, 7, 8]
    
    Output:
    [ 'Prime', 'Not Prime', 'Prime', 'Not Prime', 'Prime', 'Not Prime' ]

---
+++ newton-school-course/checking-primes-js-practice-questions/yxu5v2hfqcee.js
@@ -0,0 +1,12 @@
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true; 
+}
+
+function checkPrimes(numbers) {
+    return numbers.map(num => {isPrime(num) ? "Prime" : "Not Prime"});
+}
