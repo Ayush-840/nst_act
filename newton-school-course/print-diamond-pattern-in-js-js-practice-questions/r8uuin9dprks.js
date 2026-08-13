2f2a798c9fd9cb505299406117df82ae40322ef5
Ayush Singh <studentuse33@gmail.com>
2026-08-13 17:24:53 +0530

    Sync Solution: Print Diamond Pattern in js - JS Practice Questions [Score: 100%]
    Write a program that takes an integer n as input and prints a diamond pattern of 2n-1 height.
    
    ### Input
    First line will contain an integer n representing the number of rows in the top half of the diamond.
    
    ### Output
    Print the diamond pattern of 2n-1 height.
    
    ### Example
    Input
    5
    
    Output

--- newton-school-course/print-diamond-pattern-in-js-js-practice-questions/r8uuin9dprks.js
+++ newton-school-course/print-diamond-pattern-in-js-js-practice-questions/r8uuin9dprks.js
@@ -3,10 +3,10 @@
 
 // Top half (including middle row)
 for (let i = 1; i <= n; i++) {
-    console.log(" ".repeat(n - i) + "* ".repeat(2 * i - 1));
+    console.log(" ".repeat(n - i) + " * ".repeat(2 * i - 1));
 }
 
 // Bottom half
 for (let i = n - 1; i >= 1; i--) {
-    console.log(" ".repeat(n - i) + "* ".repeat(2 * i - 1));
+    console.log(" ".repeat(n - i) + " * ".repeat(2 * i - 1));
 }
