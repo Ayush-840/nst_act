caa8fa7e3f427ff882869ef0839bb893b22ba05e
Ayush Singh <studentuse33@gmail.com>
2026-08-17 11:36:04 +0530

    Sync Solution: Insertion into BST - Trees, Binary Tree, Binary Search Tree, Searching in BST, Insertion in BST, Inor ... - In Class [Score: 100%]
    You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
    
    Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
    
    ### Input
    User Task
    As this is a functional problem, your task is to complete the function insertIntoBST() which takes one Tree Node as its input parameters root and a integer val.
    
    Custom Input
    The first line of input contains two space separated integers, N and val, representing the number of nodes in the tree and the key value respectively.
    The next line consists of space-separated integers denoting the level order traversal of the tree, where non-negative values represent node values, and -1 indicates a null node.
    Note: Once a node is marked as -1, no further information about its children is provided.
    
    ### Output
    Return the root node of BST after insertion.
    
    Custom Output
    Function will print the level order traversal of the updated BST.
    
    ### Example
    Input
    7 25
    40 20 60 10 30 50 70
    
    Output
    40 20 60 10 30 50 70 -1 -1 25

---
+++ newton-school-course/insertion-into-bst-trees-binary-tree-binary-search-tree-searching-in-bst-insertion-in-bst-inor-in-class/xjl0zywjgl2c.py
@@ -0,0 +1,11 @@
+'''
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.left = None
+        self.right = None
+'''
+def insertIntoBST(root, val):
+    if root==None:
+        return Node(val)
+    if roo
