9f2683d4b50a21f4a7be68873309092c1c669e9e
Ayush Singh <studentuse33@gmail.com>
2026-08-18 15:54:13 +0530

    Sync Solution: Library Database Management - Schema, SQL Querying Basics, Introduction to SQL, DDL - Post Class [Score: 100%]
    A library management system is used to maintain records of books available in the library. The system stores information such as the book title, author name, genre, publication year, and the number of copies available.
    
    You need to first create a table named Books with the following structure:
    
    Books Table
    
     
     
     Column Name
     Data Type
     Constraint
     
     
     BookID
     INTEGER
     PRIMARY KEY, GENERATED BY SERIAL
     
     
     Title
     VARCHAR(100)
     -
     
     
     Author
     VARCHAR(100)
     -
     
     
     Genre
     VARCHAR(50)
     -
     
     
     PublishedYear
     INT
     -
     
     
     AvailableCopies
     INT
     -
     
     
    
    After creating the table, insert the following book record into the table:
    
     * BookID: No need to provide a value (auto-generated)
    
     * Title: "The Great Gatsby"
    
     * Author: "F. Scott Fitzgerald"
    
     * Genre: "Classic"
    
     * PublishedYear: 1925
    
     * AvailableCopies: 10
    
    Finally, write an SQL query to display all records from the Books table.
    
    Tasks to Perform:
    
     * Create the Books table.
    
     * Insert the given book record into the table.
    
     * Display all records using a SELECT query.
    
    ### Output
    CREATE TABLE
    INSERT 0 1
     bookid | title | author | genre | publishedyear | availablecopies 
    --------+------------------+---------------------+---------+---------------+-----------------
     1 | The Great Gatsby | F. Scott Fitzgerald | Classic | 1925 | 10
    (1 row)

---
+++ newton-school-course/library-database-management-schema-sql-querying-basics-introduction-to-sql-ddl-post-class/twh5hyaggqpp.js
@@ -0,0 +1,13 @@
+CREATE TABLE Books (
+    BookID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Title VARCHAR(100),
+    Author VARCHAR(100),
+    Genre VARCHAR(50),
+    PublishedYear INT,
+    AvailableCopies INT
+);
+
+INSERT INTO Books (Title, Author, Genre, PublishedYear, AvailableCopies)
+VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', 'Classic', 1925, 10);
+
+SELECT * FROM Books;
