4752ea71d6e59b65b709495804303a1d86d4a547
Ayush Singh <studentuse33@gmail.com>
2026-08-19 11:16:38 +0530

    Sync Solution: Deleting a node with 1 or 0 child in BST - Deletion In BST, Heaps - In Class [Score: 100%]
    Given the root of a Binary Search Tree and a key, delete the node with the given key in the BST. Return the root of the updated BST. 
    
    Note: It is guaranteed that the node to be deleted has at max 1 child.
    
    ### Input
    Your Task
    Since this is a functional problem, you do not need to handle input or output. Your task is to complete the function deleteFromBST(), which takes the root and an integer target as the input parameter.
    
    Custom Input
    The first line of input contains an integer, N, representing the number of nodes in the tree.
    The next line consists of space-separated integers denoting the level order traversal of the tree, where non-negative values represent node values, and -1 indicates a null node.
    The next line of input contains a single integer target, representing the target value.
    Note: Once a node is marked as -1, no further information about its children is provided.
    
    ### Output
    Return the root of the tree after deleting key from BST. The output contains level order traversal of the updated BST where number denotes node values and -1 represents null.
    
    ### Example
    Input:
    6
    5 3 6 2 4 -1 7
    7
    
    Output:
    5 3 6 2 4
    Explanation:
    Given key to delete is 7. So we find the node with value 7 and delete it.

---
+++ newton-school-course/deleting-a-node-with-1-or-0-child-in-bst-deletion-in-bst-heaps-in-class/rvhqy9i9bxec.py
@@ -0,0 +1,26 @@
+// ─── 2 ───
+'''
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right 
+'''    
+def deleteFromBST(root, target):
+    if root==None:
+        return None 
+    if target>root.val:
+        root.right=deleteFromBST(root.right,target)
+    elif target<root.val:
+        root.left=deleteFromBST(root.left,target)
+    else:
+        if root.left==None:
+            return root.right
+        if root.right==None:
+            return root.left
+    return root
+
+// ─── 3 ───
+6
+5 3 6 2 4 -1 7
+7
