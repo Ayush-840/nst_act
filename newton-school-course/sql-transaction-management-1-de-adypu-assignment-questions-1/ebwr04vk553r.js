ded9846da90891af1251eb0688878d158c340f24
Ayush Singh <studentuse33@gmail.com>
2026-08-21 04:28:06 +0530

    Sync Solution: SQL – Transaction Management 1 - DE_ADYPU_Assignment_Questions_1 [Score: 100%]
    An online shopping platform wants to safely update product stock after a customer places an order.
    
    The system should reduce the stock quantity of a product and confirm the changes only if all operations are successful.
    
    Write a PostgreSQL query to:
    
     * Start a transaction
    
     * Reduce the stock of the product Laptop by 1
    
     * Permanently save the changes
    
    Table Schema as Below:
    
    products
    
     
     
     columnName
     dataType
     
     
     
     
     product_id
     INT
     
     
     product_name
     VARCHAR(50)
     
     
     stock
     INT
     
     
     price
     INT
     
     
    
    ### Output
    Sample Output:
    
    BEGIN
    UPDATE 1
    COMMIT
    
     product_id | product_name | stock | price
    ------------+--------------+-------+-------
     2 | Mouse | 40 | 800
     3 | Keyboard | 25 | 1500
     1 | Laptop | 14 | 55000
    (3 rows)

--- newton-school-course/sql-transaction-management-1-de-adypu-assignment-questions-1/ebwr04vk553r.js
+++ newton-school-course/sql-transaction-management-1-de-adypu-assignment-questions-1/ebwr04vk553r.js
@@ -1,16 +1,4 @@
-// ─── 1 ───
 begin;
 update products set stock=stock-1 where product_name='Laptop';
 commit;
 select*from products
-
-// ─── 5 ───
-BEGIN
-UPDATE 1
-COMMIT
- product_id | product_name | stock | price 
-------------+--------------+-------+-------
-          2 | Mouse        |    40 |   800
-          3 | Keyboard     |    25 |  1500
-          1 | Laptop       |    14 | 55000
-(3 rows)
