831c50910e3c80c7583f8fc6b983db8b56425af6
Ayush Singh <studentuse33@gmail.com>
2026-08-20 23:12:01 +0530

    Sync Solution: Insert - Getting started with SQL - In Class [Score: 100%]
    You have given a table of Employees, 
    
     Employees Table
     
     
     name
     type
     primary_key
     auto_increment
     not_null
     unique
     
     
     
     
     EmployeeID
     int
     true
     true
     -
     -
     
     
     FirstName
     varchar(50)
     -
     -
     true
     -
     
     
     LastName
     varchar(50)
     -
     -
     true
     -
     
     
     Email
     varchar(100)
     -
     -
     true
     true
     
     
     HireDate
     date
     -
     -
     true
     -
     
     
     Department
     varchar(50)
     -
     -
     true
     -
     
     
    
    You need to add a new employee to the Employees table. The employee&#39;s details are:
    
     * EmployeeID: <No need to add> (PRIMARY KEY, AUTO INCREMENT)
    
     * FirstName: "Alice"
    
     * LastName: "Smith"
    
     * Email: "alice.smith@example.com"
    
     * HireDate: "2023-05-15"
    
     * Department: "HR"
    
    Write the SQL query to insert this new record.
    
    ### Output
    EmployeeID|FirstName|LastName|Email|HireDate|Department
    1|Alice|Smith|alice.smith@example.com|2023-05-15|HR

---
+++ newton-school-course/insert-getting-started-with-sql-in-class/ard6uolezoyv.js
@@ -0,0 +1,5 @@
+-- write your code here
+
+INSERT INTO Employees (FirstName, LastName, Email, HireDate, Department) VALUES 
+("Alice", "Smith", "alice.smith@example.com", "2023-05-15", "HR");
+SELECT * FROM Employees;
