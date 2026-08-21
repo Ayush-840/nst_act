4ed25adbc021a21baa57369b46b4eeef6bff1f54
Ayush Singh <studentuse33@gmail.com>
2026-08-21 22:08:33 +0530

    Sync Solution: Create Table with Constraints + Insert - DE_ADYPU_Coding_Contest_1 [Score: 100%]
    A company already has an employees table with the following columns:
    
    emp_id, emp_name, salary, department
    
    The HR department wants to modify the existing table and add information for a new employee.
    
    Task
    
    Write SQL statements to perform the following operations:
    
     * Change the datatype of the existing salary column to NUMERIC(10,2).
    
     * Add a new joining_date column of type DATE with a DEFAULT value of &#39;2026-08-19&#39;
    
     * Insert the following additional employee into the employees table without specifying the joining_date value:
    
     
     
     emp_id
     emp_name
     salary
     department
     
     
     
     
     106
     Karan
     40000
     IT
     
     
    
     
    
     * Write a query to display all records from the employees table and verify that the joining_date for the newly inserted employee has been automatically set to the &#39;2026-08-19&#39;.
    
    ### Output
    ALTER TABLE
    ALTER TABLE
    INSERT 0 1
     emp_id | emp_name | salary | department | joining_date 
    --------+----------+----------+------------+--------------
     101 | Amit | 35000.00 | IT | 2026-08-19
     102 | Neha | 42000.00 | HR | 2026-08-19
     103 | Rahul | 50000.00 | Finance | 2026-08-19
     104 | Priya | 38000.00 | IT | 2026-08-19
     105 | Sneha | 45000.00 | HR | 2026-08-19
     106 | Karan | 40000.00 | IT | 2026-08-19
    (6 rows)

--- newton-school-course/create-table-with-constraints-insert-de-adypu-coding-contest-1/89se37p2z60e.js
+++ newton-school-course/create-table-with-constraints-insert-de-adypu-coding-contest-1/89se37p2z60e.js
@@ -1,19 +1,4 @@
-// ─── 1 ───
 alter table employees alter column salary TYPE NUMERIC(10,2);
 Alter table employees add column joining_date date default '2026-08-19';
 insert into employees (emp_id,emp_name,salary,department) values (106,'Karan',40000,'IT');
 Select*from employees;
-
-// ─── 5 ───
-ALTER TABLE
-ALTER TABLE
-INSERT 0 1
- emp_id | emp_name |  salary  | department | joining_date 
---------+----------+----------+------------+--------------
-    201 | Rohan    | 37000.00 | Marketing  | 2026-08-19
-    202 | Pooja    | 46000.00 | Finance    | 2026-08-19
-    203 | Arjun    | 52000.00 | IT         | 2026-08-19
-    204 | Meera    | 41000.00 | HR         | 2026-08-19
-    205 | Vikram   | 48000.00 | Marketing  | 2026-08-19
-    106 | Karan    | 40000.00 | IT         | 2026-08-19
-(6 rows)
