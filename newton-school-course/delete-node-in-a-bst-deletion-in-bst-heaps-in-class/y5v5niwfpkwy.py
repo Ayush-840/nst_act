aed1086c065dcfab5e9dbe8cf2c7a36b8d3e818b
Ayush Singh <studentuse33@gmail.com>
2026-08-24 11:27:45 +0530

    Sync Solution: Delete Node in a BST - Deletion In BST, Heaps - In Class [Score: 100%]
    Given the root of a binary tree and a key, delete the node with the given key in the BST if it is present. Return the updated root node of the BST. 
    
    ### Input
    Your Task
    Since this is a functional problem, you do not need to handle input or output. Your task is to complete the function deleteNode(), which takes the root and key value as the input parameter.
    
    Custom Input
    The first line of input contains an integer, N, representing the number of nodes in the tree.
    The next line consists of space-separated integers denoting the level order traversal of the tree, where non-negative values represent node values, and -1 indicates a null node.
    The next line of input contains a single integer key, representing the target value to be deleted.
    Note: Once a node is marked as -1, no further information about its children is provided.
    
    ### Output
    Return the root of binary search tree after deleting key from BST. If target node is deleted from the tree it will print yes otherwise no
    
    ### Example
    Input: 
    6
    5 3 6 2 4 -1 7
    3
    Output: 
    yes
    Explanation: 
    
    The above given image depicts the reordered BST after deleting the node with value 3.
    Note: The tree can be reordered in mutiple possible ways, You can perform any of them

---
+++ newton-school-course/delete-node-in-a-bst-deletion-in-bst-heaps-in-class/y5v5niwfpkwy.py
@@ -0,0 +1,31 @@
+// ─── 2 ───
+'''
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+'''
+def deleteNode(root, key):
+    if root ==None:
+        return None
+    if key < root.val:
+        root.left=deleteNode(root.left,key)
+    elif key > root.val:
+        root.right=deleteNode(root.right,key)
+    else:
+        if root.left==None:
+            return root.right
+        if root.right==None:
+            return root.left 
+        temp=root.right
+        while temp.left:
+            temp=temp.left
+        root.val=temp.val
+        root.right=deleteNode(root.right,temp.val)
+    return root
+        
+    
+
+// ─── 8 ───
+yes
