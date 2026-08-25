338cf26ef02df74a68778ddd8555a8e9f347c614
Ayush Singh <studentuse33@gmail.com>
2026-08-25 16:26:49 +0530

    Sync Solution: Node.js CLI Calculator [Score: 100%]
    You are tasked with building a basic Command Line Interface (CLI) Calculator using Node.js. Your program should accept three command-line arguments:
    
     * (First argument) A string command representing the operation: "add", "subtract", "multiply", or "divide".
    
     * (Second argument) A number (num1).
    
     * (Third argument) A number (num2).
    
    The commands passed in the first argument should do respective arithmetic operations
    
     * "add" will do the addition of two numbers passed as arguments.
    
     * "subtract" will do the subtraction of two numbers passed as arguments.
    
     * "multiply" will do the multiplication of two numbers passed as arguments.
    
     * "divide" will do the division of two numbers passed as arguments.
    
    Your task is to parse these inputs and perform the appropriate arithmetic operation. The result should be printed to the console.
    
    Your script is executed in the following ways.
    
    Example:
    
     
     
     Input
     Output
     
     
     node index.js add 20 10
     30
     
     
     node index.js multiply 20 10
     200
     
     
     node index.js subtract 20 10
     10
     
     
     node index.js divide 20 10
     2
     
     
    
    ### Input
    A single line containing the Node.js command that will executed against index.js script with the mentioned arguments.
    
    ### Output
    The result of the arithmetic operation mentioned in the first argument.
    
    ### Example
    Input:
    node index.js add 20 10
    Output:
    30
    
    --------------------------
    
    Input:
    node index.js multiply 20 10
    Output:
    200
    
    --------------------------
    
    Input:
    node index.js subtract 20 10
    Output:
    10
    
    --------------------------
    
    Input:
    node index.js divide 20 10
    Output:
    10

---
+++ newton-school-course/node-js-cli-calculator/jjm78zvhksyk.js
@@ -0,0 +1,13 @@
+// Write your code here...
+const str = process.argv[2]
+const num1 = Number(process.argv[3])
+const num2 = Number(process.argv[4])
+if(str==="add"){
+    console.log(num1+num2)
+}else if(str==="subtract"){
+    console.log(num1-num2)
+}else if(str==="multiply"){
+    console.log( num1*num2)
+}else if(str==="divide"){
+    console.log(num1/num2)
+}
