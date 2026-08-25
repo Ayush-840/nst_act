a6fc697e52a42b740086a53ded647a93bd19e701
Ayush Singh <studentuse33@gmail.com>
2026-08-25 12:21:33 +0530

    Sync Solution: Range Sum of BST [Score: 100%]
    Given the root of a Binary Search Tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
    
    ### Input
    User Task
    Since this is a functional problem, you do not need to handle input or output. Your task is to complete the function rangeSumBST(), which takes the root node, low and high integer as the input parameter.
    
    Custom Input
    The first line of input contains an integer, N, representing the number of nodes in the tree.
    The next line consists of space-separated integers denoting the level order traversal of the tree, where non-negative values represent node values, and -1 indicates a null node.
    The third line of the input contains two integers low and high
    Note: Once a node is marked as -1, no further information about its children is provided.
    
    ### Output
    Return the sum of values of all nodes with a value greater than and equal to low and lesser than and equal to high.
    
    Custom Output
    
    ### Example
    Input:
    6
    5 3 6 2 4 -1 7
    3 6
    
    Output:
    18
     Explanation 
     3 + 4 + 5 + 6 = 18 
    
    Input:
    10
    50 25 75 12 37 62 87 -1 -1 30 -1 60 70
    30 61
    
    Output:
    177

---
+++ newton-school-course/range-sum-of-bst/n0hf45xr6sxb.py
@@ -0,0 +1,16 @@
+'''
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right 
+'''    
+def rangeSumBST(root, low, high):
+    if root==None:
+        return 0
+    total=0
+    if low <= root.val <= high:
+        total=root.val
+    total += rangeSumBST(root.left,low,high)
+    total += rangeSumBST(root.right,low,high)
+    return total
