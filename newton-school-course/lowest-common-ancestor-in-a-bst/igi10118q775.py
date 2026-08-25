79792757593df5ed75b2ec31b34684cb7b237f35
Ayush Singh <studentuse33@gmail.com>
2026-08-25 13:08:08 +0530

    Sync Solution: Lowest Common Ancestor in a BST [Score: 100%]
    Given a Binary Search Tree and two integers n1 and n2, your task is to find the lowest common ancestor (LCA) of the two given integers.
    
    The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).
    
    Note: Values n1 and n2 may or may not be present.
    
    ### Input
    Your Task
    Since this is a functional problem, you do not need to handle input or output. Your task is to complete the function LCA(), which takes the root, n1 and n2 as the input parameter.
    
    Custom Input
    The first line of input contains an integer, N, representing the number of nodes in the tree.
    The next line consists of space-separated integers denoting the level order traversal of the tree, where non-negative values represent node values, and -1 indicates a null node.
    The next line of input contains n1 and n2 representing the node values.
    Note: Once a node is marked as -1, no further information about its children is provided.
    
    ### Output
    Return the node containing LCA of n1 and n2 if both values exist otherwise None. 
    
    Custom Output
    The driver code will print the data if both n1 and n2 exist. For None, the driver code will print -1.
    
    ### Example
    Input:
    6
    5 4 6 3 -1 -1 7 -1 -1 -1 8
    7 8
    Output:
    7
    Explanation:
    
    Here the LCA of 7 and 8 is 7.
    
    Input:
    3
    5 3 6
    3 6
    Output:
    5
    Explanation:
    
    Here the LCA of 3 and 6 is 5.

---
+++ newton-school-course/lowest-common-ancestor-in-a-bst/igi10118q775.py
@@ -0,0 +1,44 @@
+// ─── 2 ───
+'''
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right 
+'''
+import sys
+sys.setrecursionlimit(10**7)   
+def LCA(node, n1, n2):
+    def ifnode(x,node):
+        if node is None:
+            return False
+        if node.val ==x:
+            return True
+        return ifnode(x,node.left) or ifnode(x,node.right)
+    def lca(node,n1,n2):
+        if node is None:
+            return None
+        if node.val == n1 or node.val==n2:
+            return node
+        left=lca(node.left,n1,n2)
+        right=lca(node.right,n1,n2)
+
+        if left and right:
+            return node
+        if left:
+            return left
+        if right:
+            return right
+        return None
+    if ifnode(n1,node) and ifnode(n2,node):
+        return lca(node,n1,n2)
+    return None
+        
+    # if n1<node.val and n2<node.val:
+    #     return LCA(node.left,n1,n2)
+    # if n1>node.val and n2>node.val:
+    #     return LCA(node.right,n1,n2)
+    # return node.val
+
+// ─── 9 ───
+7
