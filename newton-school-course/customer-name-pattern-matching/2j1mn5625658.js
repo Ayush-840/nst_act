558da7357e8642c531830b4fc6e9e15817d1dbe4
Ayush Singh <studentuse33@gmail.com>
2026-08-26 16:15:41 +0530

    Sync Solution: Customer Name Pattern Matching [Score: 100%]
    An e-commerce company wants to identify specific customer records for a promotional campaign.
    
    Write an SQL query to display the names of customers whose names:
    
     Start with the letter &#39;A&#39;
    
     Contain exactly 5 characters
    
    Return only the customer names:
    
    Table Schema as below :
    
    customers
    
     
     
     column_name
     data_type
     
     
     
     
     customer_id
     INT
     
     
     customer_name
     VARCHAR(100)
     
     
     city
     VARCHAR(50)
     
     
     membership_type
     VARCHAR(20)
     
     
    
    ### Output
     customer_name 
    ---------------
     Aarav
     Anaya
     Aditi
     Arjun
     Akash
     Aryan
     Aisha
     Ayaan
    (8 rows)

---
+++ newton-school-course/customer-name-pattern-matching/2j1mn5625658.js
@@ -0,0 +1,15 @@
+// ─── 1 ───
+select customer_name from customers where customer_name like 'A____';
+
+// ─── 5 ───
+ customer_name 
+---------------
+ Aarav
+ Anaya
+ Aditi
+ Arjun
+ Akash
+ Aryan
+ Aisha
+ Ayaan
+(8 rows)
