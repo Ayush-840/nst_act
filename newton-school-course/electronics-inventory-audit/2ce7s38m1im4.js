ac4978024e66c18aecf5eb4d2900552c5c458a94
Ayush Singh <studentuse33@gmail.com>
2026-08-26 16:11:54 +0530

    Sync Solution: Electronics Inventory Audit [Score: 100%]
    An online marketplace stores product information in the products table.
    
    A marketing analyst wants to retrieve only the products whose category is exactly &#39;Electronics&#39;.
    
    Several categories in the database contain the word Electronics as part of a larger category name, such as Home Electronics and Electronics Accessories.
    
    Write an SQL query to display the names of products that belong only to the category &#39;Electronics&#39;.
    
    Return the product name and category.
    
    Tabel schema as below:
    
    products
    
     
     
     column_name
     data_type
     
     
     
     
     product_id
     INT
     
     
     product_name
     VARCHAR(100)
     
     
     category
     VARCHAR(50)
     
     
     price
     DECIMAL(10,2)
     
     
    
    ### Output
     product_name | category 
    ----------------+-------------
     Laptop | Electronics
     Smartphone | Electronics
     Monitor | Electronics
     Gaming Console | Electronics
     Camera | Electronics
    (5 rows)

---
+++ newton-school-course/electronics-inventory-audit/2ce7s38m1im4.js
@@ -0,0 +1,14 @@
+// ─── 1 ───
+select product_name,category
+from products
+where category='Electronics'
+
+// ─── 5 ───
+  product_name  |  category   
+----------------+-------------
+ Laptop         | Electronics
+ Smartphone     | Electronics
+ Monitor        | Electronics
+ Gaming Console | Electronics
+ Camera         | Electronics
+(5 rows)
