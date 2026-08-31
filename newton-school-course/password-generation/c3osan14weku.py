f754443a188fd0e4d61ad2a2aa0786903edc9a17
Ayush Singh <studentuse33@gmail.com>
2026-08-31 12:00:47 +0530

    Sync Solution: Password Generation [Score: 100%]
    Given an integer n, generate all possible passwords of length n using the character set:
    
    chars = "aeiou1234@"
    
    Characters can be reused any number of times.
    
    Print all possible passwords.
    
    ### Input
    A single integer n, representing the length of each password.
    
    ### Output
    Print every possible password of length n. (you can print in any order)
    
    Print each password on a new line.
    
    ### Example
    Example 1:
    
    Input
    
    1
    
    Output
    
    a
    e
    i
    o
    u
    1
    2
    3
    4
    @
    
    Explanation
    For n = 1, each character from the given character set forms a password.
    
    The passwords are printed in the same order as the characters appear in:
    
    aeiou1234@

---
+++ newton-school-course/password-generation/c3osan14weku.py
@@ -0,0 +1,29 @@
+// ─── 2 ───
+# Your code here
+n=int(input())
+chars = "aeiou1234@"
+passs=[]
+def backtrac(curr):
+    if curr==n:
+        for v in passs:
+            print(v,end='')
+        print()
+        return
+    for c in chars:
+        passs.append(c)
+        backtrac(curr + 1)
+        passs.pop()
+backtrac(0)
+
+
+// ─── 9 ───
+a
+e
+i
+o
+u
+1
+2
+3
+4
+@
