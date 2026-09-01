81c4360b1ffe652a65c24ff367fec0ea02affdc9
Ayush Singh <studentuse33@gmail.com>
2026-09-02 02:02:12 +0530

    Sync Solution: Creating logs [Score: 100%]
    You are tasked with organizing a set of log files for a project. The log files need to be created for each day of the upcoming week, starting from today. The file names should follow the format log-YYYY-MM-DD.txt, where YYYY-MM-DD represents the date.
    
    A folder named "logs" is already created, you need create the log files inside this logs folder using touch command.
    
    Today is August 9th, 2024, the script should create the following files:
    
    log-2024-08-09.txt
    
    log-2024-08-10.txt
    
    log-2024-08-11.txt
    
    log-2024-08-12.txt
    
    log-2024-08-13.txt
    
    log-2024-08-14.txt
    
    log-2024-08-15.txt
    
     
    
    Write a shell script using the touch command to create these log files in the current directory.
    
    ### Input
    NA
    
    ### Output
    NA
    
    ### Example
    NA

---
+++ newton-school-course/creating-logs/apffq0k4sli9.js
@@ -0,0 +1,9 @@
+# your code goes here
+cd logs
+touch log-2024-08-09.txt
+touch log-2024-08-10.txt
+touch log-2024-08-11.txt
+touch log-2024-08-12.txt
+touch log-2024-08-13.txt
+touch log-2024-08-14.txt
+touch log-2024-08-15.txt
