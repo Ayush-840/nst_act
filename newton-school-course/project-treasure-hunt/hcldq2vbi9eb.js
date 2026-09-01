f61f27568b53d5432b22de331e85e2d025da7813
Ayush Singh <studentuse33@gmail.com>
2026-09-02 02:04:25 +0530

    Sync Solution: Project Treasure Hunt [Score: 100%]
    You receive a mysterious envelope containing an old map and a cryptic note hinting at buried treasures. Following the map leads you to a computer terminal, where you must navigate a directory to uncover the next clue.
    
    Folder structure: 
    
    home/user/treasure_hunt
    
    ├── clues
    
    │ ├── first_clue.txt
    
    │ └── second_clue.txt
    
    ├── history
    
    │ ├── year_2000
    
    │ │ └── event.txt
    
    │ └── year_1990
    
    └── notes
    
     └── secrets.txt
    
     
    Task Instructions:
    
     * Navigate to the treasure_hunt directory located inside the user directory in the home directory.
    
     * Inside the history folder, create a new directory named year_1980.
    
     * Inside the clues folder, create a new empty text file named third_clue.txt.
    
     * Inside the notes folder, create two new empty text files: reminder.txt and next_step.txt.
    
    Use relevant terminal-based commands to complete the tasks.
    
     
    Note:
    
     * Get rid of all the debugging code at last to have the test cases passed.
    
     
    
    ### Input
    NA
    
    ### Output
    NA
    
    ### Example
    NA

---
+++ newton-school-course/project-treasure-hunt/hcldq2vbi9eb.js
@@ -0,0 +1,10 @@
+# cd home/user/treasure_hunt
+# mkdir history/year_1980
+# touch clues/third_clue.txt
+# touch notes/reminder.txt notes/next_step.txt
+
+
+cd home/user/treasure_hunt
+mkdir history/year_1980
+touch clues/third_clue.txt
+touch notes/reminder.txt notes/next_step.txt
