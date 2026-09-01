8e4b366212163a38cfbc5ccd4188df629c77097d
Ayush Singh <studentuse33@gmail.com>
2026-09-02 02:08:00 +0530

    Sync Solution: Directory Manager [Score: 100%]
    You have been assigned the task of setting up the directory structure for a new project. The project requires the following directory hierarchy:
    
    project/
    
    ├── src/
    
    │ ├── components/
    
    │ ├── services/
    
    │ └── utils/
    
    ├── tests/
    
    │ ├── unit/
    
    │ └── integration/
    
    └── docs/
    
     ├── api/
    
     └── guides/
    
     
    Your task is to create this entire directory structure using a single mkdir command. Ensure that all directories are created in one go, without needing to navigate into each parent directory.
    
    Example : To create multiple nested directories, you can utilise this command format 
    
    mkdir -p /var/www/html/project/{css,js,images}
    
    This piece of code created the following directory structure : 
    
    /var/www/html/project/
    
     ├── css
    
     ├── js
    
     └── images
    
     
    
    Write the mkdir command you would use to accomplish this.
    
    ### Input
    NA
    
    ### Output
    NA
    
    ### Example
    NA

---
+++ newton-school-course/directory-manager/jpchq6w4qux3.js
@@ -0,0 +1,7 @@
+mkdir project
+mkdir project/src
+mkdir project/src/components project/src/services project/src/utils
+mkdir project/tests
+mkdir project/tests/unit project/tests/integration
+mkdir project/docs 
+mkdir project/docs/api project/docs/guides
