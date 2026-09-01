883efdfa868e8b71c22503c58f932da1962bce41
Ayush Singh <studentuse33@gmail.com>
2026-09-02 02:09:06 +0530

    Sync Solution: ProjectNST [Score: 100%]
    You have just joined a new software development team, and your first task is to add a new project called Capstone_Project to the workspace ProjectNST.
    
    The existing folder structure is:
    
    ProjectNST
    
    │
    
    ├── Dev_Project
    
    │
    
    ├── Portfolio
    
    │
    
    └── WindowSwap
    
     └── README.txt
    
    The lead developer gives you the following responsibilities:
    
     * Add the Project: Start by creating a directory named Capstone_Project for the project inside the ProjectNST workspace so all files can be neatly stored.
    
     * Provide Documentation: The team requires every project folder to include a README.txt file with a brief description.
     
     * Include README.txt file inside each project -> Portfolio, Dev_Project, and Capstone_Project. The text inside README.txt must be according to the project, in the given format, ProjectName Documentation. Eg: for Project Portfolio, README.txt file must contain Portfolio Documentation.
    
     
     
    
    ### Input
    NA
    
    ### Output
    NA
    
    ### Example
    NA

---
+++ newton-school-course/projectnst/ip3imv93wyd2.js
@@ -0,0 +1,9 @@
+mkdir ProjectNST/Capstone_Project 
+echo "Portfolio Documentation" > ProjectNST/Portfolio/README.txt
+echo "Dev_Project Documentation" > ProjectNST/Dev_Project/README.txt
+echo "Capstone_Project Documentation" > ProjectNST/Capstone_Project/README.txt
+
+# mkdir ProjectNST/Capstone_Project
+# echo "Portfolio Documentation" > ProjectNST/Portfolio/README.txt
+# echo "Dev_Project Documentation" > ProjectNST/Dev_Project/README.txt
+# echo "Capstone_Project Documentation" > ProjectNST/Capstone_Project/README.txt
