3c5aebf008a41d9a6699acd54414cfc88cd23244
Ayush Singh <studentuse33@gmail.com>
2026-09-02 02:07:40 +0530

    Sync Solution: Product Launch Modifications [Score: 100%]
    Rhea has joined the marketing team and needs to organize files for a product launch. Her tasks include updating the folder structure, creating new files, changing permissions, and deleting unnecessary files.
    
    Rhea is expected to use her terminal skills to complete the task. Can you help her organize the campaign files effectively?
    
    Rhea starts with the following folder structure located at: home/rhea/company/marketing
    
    home/rhea/company/marketing
    
     ├── ads
    
     │ ├── tv_ad.mp4
    
     │ ├── social_media_ad.mp4
    
     ├── creatives
    
     │ ├── banners
    
     │ │ └── product_banner.png
    
     │ └── logos
    
     │ └── company_logo.png
    
     ├── documents
    
     │ ├── brief.txt
    
     │ ├── marketing_plan.pdf
    
     ├── draft
    
     └── archives
    
     └── old_ads
    
     ├── old_tv_ad.mp4
    
     └── old_social_media_ad.mp4
    
     
    
     Rhea is asked to create a new document named launch_plan.txt inside the draft folder. This file should contain a rough outline of the marketing strategy with the following content:
    
     Target Audience: Young Adults and budget: $1,000,000
    
     The old_ads folder inside the archives directory contains outdated materials. The manager wants to separate them into a new folder named legacy_ads inside the archives directory. Move all files from old_ads to legacy_ads and then delete the empty old_ads folder.
    
     The creatives folder contains essential resources that should be backed up. Rhea needs to create a new folder named backup_creatives under the home/rhea/company/marketing directory. Then, copy the whole creatives folder inside the newly created backup_creatives folder, including subdirectories.
    
    Note: The final Folder Structure should look like this.
    
     home/rhea/company/marketing
    
     ├── ads
    
     │ ├── tv_ad.mp4
    
     │ └── social_media_ad.mp4
    
     ├── creatives
    
     │ ├── banners
    
     │ │ └── product_banner.png
    
     │ └── logos
    
     │ └── company_logo.png
    
     ├── backup_creatives
    
     │ └── creatives
    
     │ ├── banners
    
     │ │ └── product_banner.png
    
     │ └── logos
    
     │ └── company_logo.png
    
     ├── documents
    
     │ └── brief.txt
    
     │ └── marketing_plan.pdf
    
     ├── draft
    
     │ └── launch_plan.txt
    
     └── archives
    
     │ └── legacy_ads
    
     │ ├── old_tv_ad.mp4
    
     │ └── old_social_media_ad.mp4
    
    ### Input
    NA
    
    ### Output
    NA
    
    ### Example
    NA

---
+++ newton-school-course/product-launch-modifications/06w3grwecghh.js
@@ -0,0 +1,16 @@
+# cd home/rhea/company/marketing
+# echo "Target Audience: Young Adults and budget: $1,000,000" > draft/launch_plan.txt
+# mkdir archives/legacy_ads
+# mv archives/old_ads/* archives/legacy_ads/
+# rmdir archives/old_ads
+# mkdir backup_creatives
+# cp -r creatives backup_creatives/
+
+
+cd home/rhea/company/marketing
+echo "Target Audience: Young Adults and budget: $1,000,000" > draft/launch_plan.txt
+mkdir archives/legacy_ads
+mv archives/old_ads/* archives/legacy_ads
+rmdir archives/old_ads
+mkdir backup_creatives
+cp -r creatives backup_creatives
