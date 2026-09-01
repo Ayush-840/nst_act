fa5a2c2f38e22da7080a7dad242412bc264682c4
Ayush Singh <studentuse33@gmail.com>
2026-09-01 13:16:37 +0530

    Sync Solution: Generate Parentheses [Score: 100%]
    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
    
    ### Input
    User Task
    As this is a functional problem, your task here is to complete the function generateParenthesis which takes an integer n as it's input arguments.
    
    Custom Input
    The first and only line of the input contains a single integer n.
    
    Constraints
    1 &le; n &le; 8
    
    ### Output
    Return all the combinations of well-formed parentheses in any order.
    
    ### Example
    Sample Input
    3
    Sample Output
    ((()))
    (()())
    (())()
    ()(())
    ()()()
    Explanation
    There are total 5 combinations of well-formed parentheses using 3 pair of parentheses.

---
+++ newton-school-course/generate-parentheses/74gq4os3j6j5.py
@@ -0,0 +1,21 @@
+// ─── 2 ───
+def generateParenthesis(n):
+    res=[]
+    def ans(openn, close, s):
+        if openn == n and close == n:
+            res.append(s)
+            return
+        if openn < n:
+            ans(openn + 1, close, s + "(")   
+        if close < openn:
+            ans(openn, close + 1, s + ")")
+    ans(0,0,"")
+    return res
+
+
+// ─── 9 ───
+((()))
+(()())
+(())()
+()(())
+()()()
