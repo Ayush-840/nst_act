29b35163d12892df514e50f26e454240e047696f
Ayush Singh <studentuse33@gmail.com>
2026-09-02 10:42:43 +0530

    Sync Solution: Best Time to Buy and Sell Stock with Cooldown [Score: 100%]
    You are given an array prices where prices[i] is the price of a given stock on the ith day.
    
    Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
    
     * After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
    
    Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
    
    ### Input
    User Task
    Since this is a functional problem, you don't have to input anything. You just need to complete the maxProfit() function that takes an integer array prices as its parameter and returns an integer representing the maximum profit you can achieve.
    
    Custom Input:
    The first line of the input contains a single integer n, representing the size of the array prices.
    The second line of the input contains n space-separated integers representing elements of the array prices.
    
    ### Output
    Return an integer representing the maximum profit you can achieve.
    
    ### Example
    Sample Input
    5
    1 2 3 0 2
    
    Sample output
    3
    
    Explanation
    Transactions = [buy, sell, cooldown, buy, sell].
    Profit = (2 - 1) + (2 - 0) = 3

---
+++ newton-school-course/best-time-to-buy-and-sell-stock-with-cooldown/fjflzts54c7p.py
@@ -0,0 +1,12 @@
+def maxProfit(prices):
+    n = len(prices)
+    if n == 1:
+        return 0
+
+    b = [-float("inf")] * n
+    s = [0] * n
+    for i in range(n):
+        b[i] = max(b[i - 1], s[i - 2] - prices[i])
+        s[i] = max(s[i - 1], b[i - 1] + prices[i])
+
+    return s[n - 1]
