9b7819c8720b1a554f8dbe4ee298df4a1663b464
Ayush Singh <studentuse33@gmail.com>
2026-09-02 10:42:21 +0530

    Sync Solution: Minimum Spanning Tree - Prim's Implementation [Score: 100%]
    There are n nodes labelled from 1 to n. You are given the integer n and an array edges where edges[i] = [xi, yi, costi] indicates that the cost of connecting nodes xi and city yi (bidirectional connection) is costi.
    
    Return the minimum cost to connect all the n nodes such that there is at least one path between each pair of nodes. If it is impossible to connect all the n nodes, return -1, The cost is the sum of the edges costs used.
    
    ### Input
    User Task:
    Since this will be a functional problem, you don't have to take input. You just have to complete the function minimumCost() that takes two parameters N nodes and edges 2D array, where edges[i][0] , edges[i][1] are the nodes connected with an edges having edges[i][2] cost. 
    
    Custom Input:
    First line consists of two spaced integers n and m - number of nodes and number of edges. 
    The next m lines each contain three space-separated integers: x, y, and cost, representing an undirected edge between node x and node y with a cost of cost.
    
    ### Output
    Return a single integer minimum cost to connect all the n nodes.
    
    ### Example
    Sample Input
    3 3
    1 3 1
    1 2 5
    2 3 6
    
    Sample Output
    6
    
    Explanation
    
    Here for the generation of the minimum spanning tree, the edges which we can use are 1-2 and 1-3, leading to the resulting weight of the tree to be 6.

---
+++ newton-school-course/minimum-spanning-tree-prim-s-implementation/nzmk7vfok41m.py
@@ -0,0 +1,20 @@
+from heapq import heappush, heappop
+def minimumCost(N, edges):
+    graph = [[] for _ in range(N)]
+    for u, v, w in edges:
+        graph[u - 1].append((v - 1, w))
+        graph[v - 1].append((u - 1, w))
+    
+    heap = [(0, 0)] #weight, node
+    ans = 0
+    visited = [False] * N
+    while heap:
+        wt, node = heappop(heap)
+        if visited[node]:
+            continue 
+        ans += wt 
+        visited[node] = True
+        for nei, w in graph[node]:
+            if not visited[nei]:
+                heappush(heap, (w, nei))
+    return ans if sum(visited) == N else -1
