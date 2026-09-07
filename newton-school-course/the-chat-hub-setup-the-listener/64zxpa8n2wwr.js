f8f1c6d51594666d4ca6cdd3cc61932db6df1a81
Ayush Singh <studentuse33@gmail.com>
2026-09-07 09:43:25 +0530

    Sync Solution: The Chat Hub: Setup & The Listener [Score: 100%]
    You are building a Terminal Chat Hub using Node.js&#39;s built-in EventEmitter and readline modules.The completed application will simulate a shared chat room where multiple users can use the same terminal to send messages. In this first part, you&#39;ll set up the event system that will power the chat application.
    
    Task:
    
     Create an EventEmitter instance named chatSystem using the imported EventEmitter class.
    
     Register a listener for the &#39;messageReceived&#39; event that:
     
     Accepts an object containing username and text.
    
     Prints exactly:
     
    [Notification]: New message from [username]!
    "[text]"
     
    
     
     
    
    Note: In the next part, you&#39;ll capture user input from the terminal and emit this event.

---
+++ newton-school-course/the-chat-hub-setup-the-listener/64zxpa8n2wwr.js
@@ -0,0 +1,23 @@
+//         named chatSystem
+// TODO 2: Register a listener for 'messageReceived' event
+//         It should accept an object with username and text
+//         and print:
+//         [Notification]: New message from [username]!
+//         "[text]"
+//         ----------------------------------------
+// --- Test Code (Don't change this) ---
+// DO NOT MODIFY BELOW THIS LINE
+if (typeof chatSystem !== 'undefined') {
+    chatSystem.emit('messageReceived', {
+        username: 'TestUser',
+        text: 'Hello Chunk 1!'
+    });
+}
+module.exports = {
+    chatSystem: typeof chatSystem !== 'undefined' ? chatSystem : null
+// TODO 1: Create an EventEmitter instance
+const chatSystem=new EventEmitter();
+chatSystem.on("messageReceived",(obj)=>{
+};
+    console.log(`[Notification]: New message from ${obj.username}! ${obj.text}`)
+})
