a8e74b0c9ad23354f5c908156869eb1d11cacf0c
Ayush Singh <studentuse33@gmail.com>
2026-09-09 03:03:07 +0530

    Sync Solution: Combination Sum III [Score: 100%]
    Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
    
     1. Only numbers 1 through 9 are used.
    
     2. Each number is used at most once.
    
    Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
    
    ### Input
    User Task
    Complete the function "CombinationSumThree()", that takes two integers k and n.
    
    Custom Input
    Input consists of two integers in the same line - k & n.
    
    Constraints
    1 Input
    3 9
    
    Output
    1 2 6
    1 3 5
    2 3 4
    
    Explanation
    1 + 2 + 6 = 9
    1 + 3 + 5 = 9
    2 + 3 + 4 = 9
    There are no other valid combinations.

---
+++ newton-school-course/combination-sum-iii/vcyhctng8qty.py
@@ -0,0 +1,18 @@
+def combinationSum3(k, n):
+    ans=[]
+    path=[]
+    def back(i,target):
+        if len(path)==k:
+            if target==0:
+                ans.append(path[:])
+                return
+        if i==10:
+            return
+        if i <= target:
+            path.append(i)
+            back(i+1,target-i)
+            path.pop()
+        back(i+1,target)
+            
+    back(1,n)
+    return ans
