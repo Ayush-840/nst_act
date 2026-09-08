5ecbe61a3073678aa8b45cb990d02a9aa2bca498
Ayush Singh <studentuse33@gmail.com>
2026-09-09 02:00:17 +0530

    Sync Solution: Subsets (II) [Score: 100%]
    Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
    
    The solution set must not contain duplicate subsets. Return the solution in any order.
    
    ### Input
    User Task:
    Since this will be a functional problem, you don't have to take input. You just have to complete the function subsetsWithDup() that takes Integer array "nums" as parameter.
    
    Custom Input
    The first line of the input contains integer n, representing the size of the array "nums"
    The second line of the input contains n space separated integers representing elements of the array "nums".
    
    Constraints:
    1 Custom Output:
    If the list of subsets matches with the output then "Yes" will be printed else "No" will be printed.
    
    ### Example
    Input: 
    3
    1 2 2
    Output: 
    Yes
    Explanation:
    [[],[1],[1,2],[1,2,2],[2],[2,2]]

---
+++ newton-school-course/subsets-ii/j8451ipwlln6.py
@@ -0,0 +1,21 @@
+// ─── 2 ───
+def subsetsWithDup(nums):
+    nums.sort()
+    ans=[]
+    path=[]
+    def back(i):
+        ans.append(path[:])
+        for ch in range(i,len(nums)):
+            if ch>i and nums[ch]==nums[ch-1]:
+                continue
+            path.append(nums[ch])
+            back(ch+1)
+            path.pop()
+    back(0)
+    return ans
+
+
+
+
+// ─── 8 ───
+[[],[1],[1,2],[1,2,2],[2],[2,2]]
