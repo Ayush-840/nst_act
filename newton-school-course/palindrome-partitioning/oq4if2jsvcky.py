348113cf75c084aadb6e8c30160188314a646f3a
Ayush Singh <studentuse33@gmail.com>
2026-09-10 12:28:34 +0530

    Sync Solution: Palindrome Partitioning [Score: 100%]
    Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
    
    ### Input
    Since this is a functional problem, just implement the function partition.
    The first line of input contains string s.
    Constraints
    
    * 1 
    
    ### Output
    Return all possible palindrome partitioning of s.
    
    Custom Output:
    We will compare the correct palindrome partitions with the returned palindrome partitions. We will print all possible answers.
    
    ### Example
    Input:
    aab
    Output: 
    [["a","a","b"],["aa","b"]]
    
    Explanation:
    The possible palindrome partitions are ["a","a","b"] and ["aa","b"].
    We will match the correct palindrome partitions with the returned palindrome partitions.
    
    Example 2:
    
    Input:
    a
    Output: 
    [["a"]]
    
    Explanation:
    The possible palindrome partitions are ["a"]

---
+++ newton-school-course/palindrome-partitioning/oq4if2jsvcky.py
@@ -0,0 +1,21 @@
+// ─── 2 ───
+def palindrome_partitions(s):
+    ans=[]
+    path=[]
+    def back(i):
+        if i==len(s):
+            ans.append(path[:])
+            return
+        for j in range(i,len(s)):
+            part=s[i:j+1]
+            if part==part[::-1]:
+                path.append(part)
+                back(j+1)
+                path.pop()
+    back(0)
+    return ans
+        
+    
+
+// ─── 7 ───
+[["a","a","b"],["aa","b"]]
