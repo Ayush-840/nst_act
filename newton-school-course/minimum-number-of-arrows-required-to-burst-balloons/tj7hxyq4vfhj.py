c2ff47222b160d9997c38a546a1424efec68ed8c
Ayush Singh <studentuse33@gmail.com>
2026-09-14 12:39:20 +0530

    Sync Solution: Minimum Number of Arrows required to Burst Balloons [Score: 100%]
    There are some spherical balloons taped onto a flat wall that represents the XY-plane. The balloons are represented as a 2D integer array points where points[i] = [xstart, xend] denotes a balloon whose horizontal diameter stretches between xstart and xend. You do not know the exact y-coordinates of the balloons.
    
    Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis. A balloon with xstart and xend is burst by an arrow shot at x if xstart <= x <= xend. There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
    
    Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
    
    ### Input
    You don't need to read inputs or print anything. Complete the function findMinArrowShots() that takes a 2D array points and returns an integer representing the minimum number of arrows that must be shot to burst all balloons.
    
    Custom Input:
    The first line will take an integer n as input representing the number of balloons.
    Next n lines will take two space-separated integers representing the elements of the points array.
    
    Constraints
    1 &le; points.length &le; 105
    points[i].length == 2
    -231 &le; xstart end &le; 231 - 1
    
    ### Output
    Return an integer representing the minimum number of arrows that must be shot to burst all balloons.
    
    ### Example
    Input: 
    4
    10 16
    2 8
    1 6
    7 12
    Output: 
    2
    Explanation: 
    points = [[10,16],[2,8],[1,6],[7,12]]
    The balloons can be burst by 2 arrows:
    - Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
    - Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
    
    Input:
    4
    1 2
    3 4
    5 6
    7 8
    Output: 
    4
    Explanation: 
    points = [[1,2],[3,4],[5,6],[7,8]]
    One arrow needs to be shot for each balloon for a total of 4 arrows.

---
+++ newton-school-course/minimum-number-of-arrows-required-to-burst-balloons/tj7hxyq4vfhj.py
@@ -0,0 +1,13 @@
+def findMinArrowShots(points):
+    points.sort()
+    limit=points[0][1]
+    ans=1
+    for i in range(1, len(points)):
+        curr=points[i][0]   
+        if(curr<=limit):
+            if(points[i][1]<limit):
+                limit = points[i][1]
+        else:
+            ans+=1
+            limit = points[i][1]  
+    return ans
