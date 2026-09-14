3af2da6eac06eef4eb1023a7781581afa875d9c0
Ayush Singh <studentuse33@gmail.com>
2026-09-14 12:39:13 +0530

    Sync Solution: Minimum Number of Increments on Subarrays to Form a Target Array [Score: 100%]
    You are given an integer array target. You have an integer array initial of the same size as target with all elements initially zeros.
    
    In one operation you can choose any subarray from initial and increment each value by one.
    
    Return the minimum number of operations to form a target array from initial.
    
    The test cases are generated so that the answer fits in a 32-bit integer.
    
    ### Input
    You don't need to read inputs or print anything. Complete the function minNumberOperations() that takes an array representing target as input parameters and returns an integer representing the minimum number of operations to form a target array from the initial.
    
    Custom input:
    The first line of input takes an integer n representing the size of the array target.
    The second line of input will take n space-separated integers representing elements of array target.
    
    Constraints:
    1 &le; target.length &le; 105
    1 &le; target[i] &le; 105
    
    ### Output
    Return an integer representing the minimum number of operations to form a target array from the initial.
    
    ### Example
    Input: 
    5
    1 2 3 2 1
    Output: 
    3
    Explanation:
    target = [1,2,3,2,1] 
    We need at least 3 operations to form the target array from the initial array.
    [0,0,0,0,0] increment 1 from index 0 to 4 (inclusive).
    [1,1,1,1,1] increment 1 from index 1 to 3 (inclusive).
    [1,2,2,2,1] increment 1 at index 2.
    [1,2,3,2,1] target array is formed.
    
    Input: 
    4
    3 1 1 2
    Output: 
    4
    Explanation: 
    target = [3,1,1,2]
    [0,0,0,0] -> [1,1,1,1] -> [1,1,1,2] -> [2,1,1,2] -> [3,1,1,2]

---
+++ newton-school-course/minimum-number-of-increments-on-subarrays-to-form-a-target-array/0t3lzxuv9nig.py
@@ -0,0 +1,11 @@
+def minNumberOperations(target):
+    n=len(target)
+    prev=0
+    ans=0
+    for i in range(n):
+        if(target[i]<=prev):
+            prev=target[i]
+        else:
+            ans+=abs(target[i]-prev)
+            prev=target[i]
+    return ans
