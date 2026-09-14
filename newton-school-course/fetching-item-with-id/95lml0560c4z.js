fceee8b215f3a057b3948e3554a3299ee875a694
Ayush Singh <studentuse33@gmail.com>
2026-09-14 23:56:57 +0530

    Sync Solution: Fetching item with id [Score: 100%]
    Create a Node.js server which:
    
     
     If GET / is called, responds with &ldquo;Home page&rdquo;
    
     
    
     
     If GET /item/:id (for example /item/123) is called, extract the id from the URL manually and respond with &ldquo;Fetching item with id 123&rdquo;
    
     
    
     
     If the route is not recognized, respond with a 404 status code and the text "404 Not Found".

---
+++ newton-school-course/fetching-item-with-id/95lml0560c4z.js
@@ -0,0 +1,20 @@
+});
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+  if(req.method==="GET" && req.url==="/"){
+    res.writeHead(200,{"content-Type":"text/plain"});
+  }
+    res.end("Home page");
+  // Your code here
+  // }
+    res.writeHead(404,{"content-type":"text/plain"});
+  }
+    res.end("404 Not Found");
+  //   const id= req.url.split("/")[2];
+  //   res.writeHead(200,{"content-type":"text/plain"})
+  //   res.end(`Fetching item with id ${id}`)
+});
+const server = http.createServer((req, res) => {
+  // else if(req.method==="GET" && req.url.startsWith("/item/")){
+  else{
+module.exports = { server };
